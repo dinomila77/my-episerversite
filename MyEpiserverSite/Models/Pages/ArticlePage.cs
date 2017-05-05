41644d520babc5986d2c2e6e536b884ed938fa42
Michel Miladinovic <miladino77@hotmail.com>
2017-05-05 15:44:26 +0200

    Adds Start&Article pages

---
+++ MyEpiserverSite/Models/Pages/ArticlePage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "ArticlePage", GUID = "c9f58b29-caa7-4749-b571-3c9a6da2d758", Description = "Basic page type for creating articles.")]
+    public class ArticlePage : SitePageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body editor area lets you insert text, images into a page.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
---
+++ MyEpiserverSite/Models/Pages/StartPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "ef772315-bbc6-4840-bb0e-ada8ceac71ba", Description = "Site start page")]
+    public class StartPage : SitePageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}
