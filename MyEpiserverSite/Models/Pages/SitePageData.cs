f8c41dd3ea9e3f52d1e2d8f89ac77800b643219c
Michel Miladinovic <miladino77@hotmail.com>
2017-05-05 15:36:21 +0200

    Make class abstract

--- MyEpiserverSite/Models/Pages/SitePageData.cs
+++ MyEpiserverSite/Models/Pages/SitePageData.cs
@@ -8,7 +8,7 @@ using EPiServer.SpecializedProperties;
 namespace MyEpiserverSite.Models.Pages
 {
     //[ContentType(DisplayName = "SitePageData", GUID = "2dff107a-a5a0-40d6-92ba-8ccc9f507d72", Description = "")]
-    public class SitePageData : PageData
+    public abstract class SitePageData : PageData
     {
         [Display(GroupName = "SEO", Order = 200, Name = "Search keywords")]
         public virtual string MetaKeywords { get; set; }
