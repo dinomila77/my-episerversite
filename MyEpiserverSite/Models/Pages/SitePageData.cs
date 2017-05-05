09373b622b54bed2c25fbe3f5ded1018d6ec2174
Michel Miladinovic <miladino77@hotmail.com>
2017-05-05 15:34:42 +0200

    Adds SitePageData

---
+++ MyEpiserverSite/Models/Pages/SitePageData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    //[ContentType(DisplayName = "SitePageData", GUID = "2dff107a-a5a0-40d6-92ba-8ccc9f507d72", Description = "")]
+    public class SitePageData : PageData
+    {
+        [Display(GroupName = "SEO", Order = 200, Name = "Search keywords")]
+        public virtual string MetaKeywords { get; set; }
+    }
+}
