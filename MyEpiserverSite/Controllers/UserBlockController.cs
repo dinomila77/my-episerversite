dbcf50c7c11935f748942c88e98a64b8b0f7636c
Michel Miladinovic <miladino77@hotmail.com>
2017-05-09 16:37:34 +0200

    Adds UserBlockController

---
+++ MyEpiserverSite/Controllers/UserBlockController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Web;
+using EPiServer.Web.Mvc;
+using MyEpiserverSite.Models.Blocks;
+
+namespace MyEpiserverSite.Controllers
+{
+    public class UserBlockController : BlockController<UserBlock>
+    {
+        public override ActionResult Index(UserBlock currentBlock)
+        {
+            return PartialView(currentBlock);
+        }
+    }
+}
