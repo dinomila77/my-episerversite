a1e3071da586ec3cdf627c2cb15168ca3f5446d1
Michel Miladinovic <miladino77@hotmail.com>
2017-05-09 15:17:53 +0200

    Commented line in gitignore, adds folder to repository

---
+++ MyEpiserverSite/Models/Media/ImageFile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web;
+
+namespace MyEpiserverSite.Models.Media
+{
+    [ContentType(DisplayName = "ImageFile", GUID = "05019e32-c56d-4b75-ac44-2015bf71c919", Description = "")]
+    [MediaDescriptor(ExtensionString = "jpg,jpeg,jpe,gif,bmp,png,ico")]
+    public class ImageFile : ImageData
+    {
+        [Display(
+            Name = "Description",
+            Description = "Description field's description",
+            GroupName = SystemTabNames.Content,
+            Order = 20)]
+        public virtual string Copyright { get; set; }
+
+    }
+}
