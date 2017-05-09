5958547b172ee9a5e27b403f2210dc02cd8c18d6
Michel Miladinovic <miladino77@hotmail.com>
2017-05-09 16:31:46 +0200

    Adds UserBlock ViewModels

---
+++ MyEpiserverSite/Models/ViewModels/UserBasicBlockModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyEpiserverSite.Models.ViewModels
+{
+    public class UserBasicBlockModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Personal number")]
+        public string PersonalNumber { get; set; }
+    }
+}
---
+++ MyEpiserverSite/Models/ViewModels/UserDetailsBlockModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyEpiserverSite.Models.ViewModels
+{
+    public class UserDetailsBlockModel
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Zip code")]
+        public string ZipCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
