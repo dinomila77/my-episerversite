946deeecec12c819d38301db0857bd4fdb2411b5
Michel Miladinovic <miladino77@hotmail.com>
2017-05-09 15:48:12 +0200

    Adds ContactPage

---
+++ MyEpiserverSite/Models/Pages/ContactPage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "ContactPage", GUID = "7004aa25-81fa-4b68-81dc-28d0cf3ef166", Description = "")]
+    public class ContactPage : SitePageData
+    {
+
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
