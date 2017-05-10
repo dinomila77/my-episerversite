3f497bb3b31b29420fb7b6c2d5bb2ad71c5b6e8e
Michel Miladinovic <miladino77@hotmail.com>
2017-05-10 11:24:14 +0200

    Adds basic code to UserBlockController

--- MyEpiserverSite/Controllers/UserBlockController.cs
+++ MyEpiserverSite/Controllers/UserBlockController.cs
@@ -8,6 +8,7 @@ using EPiServer.Core;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 using MyEpiserverSite.Models.Blocks;
+using MyEpiserverSite.Models.ViewModels;
 
 namespace MyEpiserverSite.Controllers
 {
@@ -15,7 +16,45 @@ namespace MyEpiserverSite.Controllers
     {
         public override ActionResult Index(UserBlock currentBlock)
         {
-            return PartialView(currentBlock);
+            return PartialView();
+        }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        [HttpPost]
+        public ActionResult UserBasic(UserBasicViewModel userBasic, string btnNext)
+        {
+            if (btnNext != null)
+            {
+                if (ModelState.IsValid && Request.IsAjaxRequest())
+                {
+                    UserBlock userBlock = GetUser();
+                    userBlock.Email = userBasic.Email;
+                    userBlock.PersonalNumber = userBasic.PersonalNumber;
+
+                    return PartialView("_UserDetails");
+                }
+            }
+
+            return PartialView("_UserBasic");
+        }
+
+        public ActionResult UserDetails(UserDetailsViewModel userDetails, string btnPrev, string btnSave)
+        {
+            
+            return PartialView("_UserDetails");
+        }
+
+        private UserBlock GetUser()
+        {
+            if (Session["user"] == null)
+            {
+                Session["user"] = new UserBlock();
+            }
+            return (UserBlock) Session["user"];
         }
     }
 }
