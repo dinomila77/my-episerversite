d9d359ec1d0d1071221223aa27af8a07874f9c1a
Michel Miladinovic <miladino77@hotmail.com>
2017-05-11 11:13:37 +0200

    Adds new class UserEntity

---
+++ MyEpiserverSite/Models/Entities/UserEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyEpiserverSite.Models.Entities
+{
+    public class UserEntity
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public virtual string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public virtual string LastName { get; set; }
+
+        [Required]
+        public virtual string Address { get; set; }
+
+        [Required]
+        [Display(Name = "Zip code")]
+        public virtual string ZipCode { get; set; }
+
+        [Required]
+        public virtual string City { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public virtual string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Personal number")]
+        public virtual string PersonalNumber { get; set; }
+
+        [Display(Name = "Phone number")]
+        public virtual string PhoneNumber { get; set; }
+    }
+}
