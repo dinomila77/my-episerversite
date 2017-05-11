331b27ce98b479dbc8ff77c61650bdf1d86740f3
Michel Miladinovic <miladino77@hotmail.com>
2017-05-11 11:12:40 +0200

    Modifies UserBlock

--- MyEpiserverSite/Models/Blocks/UserBlock.cs
+++ MyEpiserverSite/Models/Blocks/UserBlock.cs
@@ -4,48 +4,76 @@ using System.ComponentModel.DataAnnotations;
 using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
+using MyEpiserverSite.Models.Entities;
 
 namespace MyEpiserverSite.Models.Blocks
 {
-    [ContentType(DisplayName = "UserBlock", GUID = "cc4b246e-4c66-4907-8a8d-92061e3ec78a", Description = "")]
+    [ContentType(DisplayName = "User Block", GUID = "cc4b246e-4c66-4907-8a8d-92061e3ec78a", Description = "Block for registering user")]
     public class UserBlock : SiteBlockData
     {
+        [Ignore]
+        public UserEntity User { get; set; }
 
-        //[CultureSpecific]
-        //[Display(
-        //    Name = "Name",
-        //    Description = "Name field's description",
-        //    GroupName = SystemTabNames.Content,
-        //    Order = 1)]
-        //public virtual string Name { get; set; }
+        public UserBlock()
+        {
+            User = new UserEntity();
+        }
+
+        [CultureSpecific]
+        [Required]
+        [Display(
+            Name = "Heading",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual string Heading { get; set; }
 
         [Required]
-        [Display(Name ="First name")]
+        [Display(
+            Name = "First Name label",
+            GroupName = SystemTabNames.Content,
+            Order = 2)]
         public virtual string FirstName { get; set; }
 
         [Required]
-        [Display(Name = "Last name")]
+        [Display(Name = "Last name label",
+            GroupName = SystemTabNames.Content,
+            Order = 3)]
         public virtual string LastName { get; set; }
 
         [Required]
+        [Display(Name = "Address label",
+            GroupName = SystemTabNames.Content,
+            Order = 4)]
         public virtual string Address { get; set; }
 
         [Required]
-        [Display(Name = "Zip code")]
+        [Display(Name = "Zip code label",
+            GroupName = SystemTabNames.Content,
+            Order = 5)]
         public virtual string ZipCode { get; set; }
 
         [Required]
+        [Display(Name = "City label",
+            GroupName = SystemTabNames.Content,
+            Order = 6)]
         public virtual string City { get; set; }
 
         [Required]
-        [EmailAddress]
+        [Display(Name = "Email address label",
+            GroupName = SystemTabNames.Content,
+            Order = 7)]
         public virtual string Email { get; set; }
 
         [Required]
-        [Display(Name = "Personal number")]
+        [Display(Name = "Personal number label",
+            GroupName = SystemTabNames.Content,
+            Order = 8)]
         public virtual string PersonalNumber { get; set; }
 
-        [Display(Name = "Phone number")]
+        [Required]
+        [Display(Name = "Phone number label",
+            GroupName = SystemTabNames.Content,
+            Order = 9)]
         public virtual string PhoneNumber { get; set; }
     }
 }
