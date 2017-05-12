372754aaef10990865ed5db2c641c8d97a4a2fa6
Michel Miladinovic <miladino77@hotmail.com>
2017-05-12 10:15:50 +0200

    GetUser method returns UserEntity

--- MyEpiserverSite/Controllers/UserBlockController.cs
+++ MyEpiserverSite/Controllers/UserBlockController.cs
@@ -8,6 +8,7 @@ using EPiServer.Core;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 using MyEpiserverSite.Models.Blocks;
+using MyEpiserverSite.Models.Entities;
 using MyEpiserverSite.Models.ViewModels;
 
 namespace MyEpiserverSite.Controllers
@@ -19,11 +20,6 @@ namespace MyEpiserverSite.Controllers
             return PartialView();
         }
 
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
         [HttpPost]
         public ActionResult UserBasic(UserBasicViewModel userBasic, string btnNext)
         {
@@ -31,15 +27,15 @@ namespace MyEpiserverSite.Controllers
             {
                 if (ModelState.IsValid && Request.IsAjaxRequest())
                 {
-                    UserBlock userBlock = GetUser();
-                    userBlock.Email = userBasic.Email;
-                    userBlock.PersonalNumber = userBasic.PersonalNumber;
+                    UserEntity userEntity = GetUser();
+                    userEntity.Email = userBasic.Email;
+                    userEntity.PersonalNumber = userBasic.PersonalNumber;
 
                     return PartialView("_UserDetails");
                 }
             }
-
-            return PartialView("_UserBasic");
+            return View("Index");
+            //return PartialView("_UserBasic");
         }
 
         public ActionResult UserDetails(UserDetailsViewModel userDetails, string btnPrev, string btnSave)
@@ -48,13 +44,13 @@ namespace MyEpiserverSite.Controllers
             return PartialView("_UserDetails");
         }
 
-        private UserBlock GetUser()
+        private UserEntity GetUser()
         {
             if (Session["user"] == null)
             {
-                Session["user"] = new UserBlock();
+                Session["user"] = new UserEntity();
             }
-            return (UserBlock) Session["user"];
+            return (UserEntity)Session["user"];
         }
     }
 }
