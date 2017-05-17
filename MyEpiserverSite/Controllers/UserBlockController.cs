12012243424f6ae69996f810edd5799b893b4b67
Michel Miladinovic <miladino77@hotmail.com>
2017-05-17 09:57:10 +0200

    Adds methods in UserBlockController for Wizard functionality

--- MyEpiserverSite/Controllers/UserBlockController.cs
+++ MyEpiserverSite/Controllers/UserBlockController.cs
@@ -5,11 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 using EPiServer;
 using EPiServer.Core;
+using EPiServer.Framework.Serialization.Json.Internal;
 using EPiServer.Web;
 using EPiServer.Web.Mvc;
 using MyEpiserverSite.Models.Blocks;
 using MyEpiserverSite.Models.Entities;
 using MyEpiserverSite.Models.ViewModels;
+using MyEpiserverSite.Utilities;
+using Newtonsoft.Json;
 
 namespace MyEpiserverSite.Controllers
 {
@@ -25,32 +28,103 @@ namespace MyEpiserverSite.Controllers
         {
             if (btnNext != null)
             {
-                if (ModelState.IsValid && Request.IsAjaxRequest())
+                if (ModelState.IsValid)
                 {
-                    UserEntity userEntity = GetUser();
+                    var userEntity = GetUserEntity();
                     userEntity.Email = userBasic.Email;
                     userEntity.PersonalNumber = userBasic.PersonalNumber;
 
-                    return PartialView("_UserDetails");
+                    var userDetails = GetUserDetails(userEntity);
+                    return PartialView("_UserDetails",userDetails);
                 }
             }
-            return View("Index");
-            //return PartialView("_UserBasic");
+            return PartialView("_UserBasic");
         }
 
+        [HttpPost]
         public ActionResult UserDetails(UserDetailsViewModel userDetails, string btnPrev, string btnSave)
         {
+            var userEntity = GetUserEntity();
+            if (btnPrev != null)
+            {
+                StoreUserDetailsToUserEntity(userDetails);
+                var userBasic = new UserBasicViewModel
+                {
+                    Email = userEntity.Email,
+                    PersonalNumber = userEntity.PersonalNumber
+                };
+                ModelState.Clear();
+                return PartialView("_UserBasic",userBasic);
+            }
+
+            if (btnSave == null) return PartialView("_UserDetails");
+
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    StoreUserDetailsToUserEntity(userDetails);
+                    const string path = @"C:\Users\Shkomi\Documents\Temp\epiusers.json";
+                    const string errorPath = @"C:\dadsakfak\users.json";
+                    var lines = PersistenceUtility.TextToFile(JsonConvert.SerializeObject(userEntity, Formatting.Indented));
+                    System.IO.File.AppendAllLines(path, lines);
+                    //TempData["success"] = "Thank you for your sumbission.";
+                    TempData["success"] = true;
+                    RemoveUserEntity();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("ErrorSavingToDisk", $"{ex.Message} Please try again.");
+
+                var errorMessage = PersistenceUtility.TextToFile($"Error: {ex.Message}");
+                const string logPath = @"C:\Users\Shkomi\Documents\Temp\epierror.log";
+                System.IO.File.AppendAllLines(logPath, errorMessage);
+                return PartialView("_UserDetails");
+            }
             
             return PartialView("_UserDetails");
         }
 
-        private UserEntity GetUser()
+        private UserEntity GetUserEntity()
         {
             if (Session["user"] == null)
             {
                 Session["user"] = new UserEntity();
             }
             return (UserEntity)Session["user"];
+        }
+
+        private void RemoveUserEntity()
+        {
+            Session.Remove("user");
+        }
+
+        private UserDetailsViewModel GetUserDetails(UserEntity userEntity)
+        {
+            var userDetails = new UserDetailsViewModel
+            {
+                FirstName = userEntity.FirstName,
+                LastName = userEntity.LastName,
+                Address = userEntity.Address,
+                City = userEntity.City,
+                PhoneNumber = userEntity.PhoneNumber,
+                ZipCode = userEntity.ZipCode
+            };
+            return userDetails;
+        }
+
+        private void StoreUserDetailsToUserEntity(UserDetailsViewModel userDetails)
+        {
+            var userEntity = GetUserEntity();
+
+            userEntity.FirstName = userDetails.FirstName;
+            userEntity.LastName = userDetails.LastName;
+            userEntity.Address = userDetails.Address;
+            userEntity.ZipCode = userDetails.ZipCode;
+            userEntity.PhoneNumber = userDetails.PhoneNumber;
+            userEntity.City = userDetails.City;
         }
     }
 }
