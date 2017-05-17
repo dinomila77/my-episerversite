66ad0a09a710008534b8ca23d016919d5653aafb
Michel Miladinovic <miladino77@hotmail.com>
2017-05-17 10:12:27 +0200

    Adds PersistanceUtility class to project

---
+++ MyEpiserverSite/Utilities/PersistenceUtility.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace MyEpiserverSite.Utilities
+{
+    public class PersistenceUtility
+    {
+        public static List<string> TextToFile(string input)
+        {
+            var builder = new StringBuilder();
+            var dateNow = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss  ");
+            builder.AppendLine($"Date: {dateNow}").AppendLine(input);
+            var text = builder.ToString();
+            var textToFile = new List<string> { text };
+
+            return textToFile;
+        }
+    }
+}
