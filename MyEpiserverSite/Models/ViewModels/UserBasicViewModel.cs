b1ba29b102f566bd7ae28270615f184bbd689609
Michel Miladinovic <miladino77@hotmail.com>
2017-05-18 11:26:17 +0200

    Adds personal number validation

--- MyEpiserverSite/Models/ViewModels/UserBasicViewModel.cs
+++ MyEpiserverSite/Models/ViewModels/UserBasicViewModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 
 namespace MyEpiserverSite.Models.ViewModels
 {
-    public class UserBasicViewModel
+    public class UserBasicViewModel : IValidatableObject
     {
         [Required]
         [EmailAddress]
@@ -15,5 +15,27 @@ namespace MyEpiserverSite.Models.ViewModels
         [Required]
         [Display(Name = "Personal number")]
         public string PersonalNumber { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!IsValidPersonalNumber(PersonalNumber))
+            {
+                yield return new ValidationResult("The personal number you've entered is not valid.");
+            }
+        }
+
+        private static bool IsValidPersonalNumber(string identityNumber)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < identityNumber.Length; i++)
+            {
+                int n = identityNumber[i] - '0';
+                if (i % 2 == 0)
+                    n = n * 2 > 9 ? n * 2 - 9 : n * 2;
+                sum += n;
+            }
+            return sum % 10 == 0;
+        }
     }
 }
