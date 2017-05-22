0ce89b7734b6ea443f3913298a2a0d6ecfee6637
Michel Miladinovic <miladino77@hotmail.com>
2017-05-22 15:46:01 +0200

    Adds interface IPageViewModel

---
+++ MyEpiserverSite/Models/ViewModels/IPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyEpiserverSite.Models.Pages;
+
+namespace MyEpiserverSite.Models.ViewModels
+{
+    public interface IPageViewModel<out T> where T : SitePageData
+    {
+        T CurrentPage { get; }
+    }
+}
