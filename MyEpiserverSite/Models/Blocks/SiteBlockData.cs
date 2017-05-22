ae279c1ee502b5f233c1cd3dbc92eec19eb12622
Michel Miladinovic <miladino77@hotmail.com>
2017-05-22 10:42:06 +0200

    Commented code for getting current page testing

--- MyEpiserverSite/Models/Blocks/SiteBlockData.cs
+++ MyEpiserverSite/Models/Blocks/SiteBlockData.cs
@@ -8,13 +8,13 @@ namespace MyEpiserverSite.Models.Blocks
 {
     public class SiteBlockData : BlockData
     {
-        public PageData CurrentPage
-        {
-            get
-            {
-                var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
-                return pageRouteHelper.Page;
-            }
-        }
+        //public PageData CurrentPage
+        //{
+        //    get
+        //    {
+        //        var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
+        //        return pageRouteHelper.Page;
+        //    }
+        //}
     }
 }
