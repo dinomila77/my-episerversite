0b1296750846118e8ad551157fe0c9adeb4684f5
Michel Miladinovic <miladino77@hotmail.com>
2017-05-24 09:24:57 +0200

    Adds new page type CreateContent

---
+++ MyEpiserverSite/Models/Pages/CreateContentPage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace MyEpiserverSite.Models.Pages
+{
+    [ContentType(DisplayName = "CreateContentPage", GUID = "8d2c2dee-e7d5-49b4-9781-f9ce4efb3b9a", Description = "")]
+    public class CreateContentPage : StandardPage
+    {
+        
+    }
+}
