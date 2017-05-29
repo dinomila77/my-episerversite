7fbd765b586a32e98fbd86d6ae3de361189ee388
Michel Miladinovic <miladino77@hotmail.com>
2017-05-29 09:21:15 +0200

    CreateContentPage inherits now from SitePageData

--- MyEpiserverSite/Models/Pages/CreateContentPage.cs
+++ MyEpiserverSite/Models/Pages/CreateContentPage.cs
@@ -4,12 +4,18 @@ using EPiServer.Core;
 using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using EPiServer.SpecializedProperties;
+using EPiServer.Web;
 
 namespace MyEpiserverSite.Models.Pages
 {
     [ContentType(DisplayName = "CreateContentPage", GUID = "8d2c2dee-e7d5-49b4-9781-f9ce4efb3b9a", Description = "")]
-    public class CreateContentPage : StandardPage
+    public class CreateContentPage : SitePageData
     {
-        
+        [UIHint(UIHint.Textarea)]
+        public virtual string Introduction { get; set; }
+
+        [AllowedTypes(typeof(StandardPage))]
+        [Display(Name = "Parent Id")]
+        public virtual ContentReference ParentId { get; set; }
     }
 }
