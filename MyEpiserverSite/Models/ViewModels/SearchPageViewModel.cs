e0da8e948975613367325d10b86fc19c9b229d50
Michel Miladinovic <miladino77@hotmail.com>
2017-05-30 09:26:31 +0200

    Adds SearchPageViewModel

---
+++ MyEpiserverSite/Models/ViewModels/SearchPageViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Search;
+using EPiServer.ServiceLocation;
+using MyEpiserverSite.Models.Pages;
+
+namespace MyEpiserverSite.Models.ViewModels
+{
+    public class SearchPageViewModel : PageViewModel<SearchPage>
+    {
+        private string _searchTerm;
+        public IEnumerable<PageData> SearchResults { get; set; }
+        public bool HasSearchResult { get; set; }
+
+        public SearchPageViewModel(SearchPage currentPage) : base(currentPage)
+        {
+        }
+
+        public SearchPageViewModel(SearchPage currentPage, string searchTerm) : this(currentPage)
+        {
+            _searchTerm = searchTerm;
+
+            if (!string.IsNullOrEmpty(_searchTerm))
+            {
+                PropertyCriteriaCollection criterias = new PropertyCriteriaCollection
+                {
+                    new PropertyCriteria()
+                    {
+                        Name = "PageName",
+                        Type = PropertyDataType.String,
+                        Condition = EPiServer.Filters.CompareCondition.Equal,
+                        Value = _searchTerm
+                    }
+                };
+
+                var repository = ServiceLocator.Current.GetInstance<IPageCriteriaQueryService>();
+
+                SearchResults = repository.FindPagesWithCriteria(ContentReference.StartPage, criterias);
+            }
+
+            HasSearchResult = SearchResults != null;
+        }
+
+        
+    }
+}
