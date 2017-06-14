1c7ee63a9272c773c97ea668dc092dc1689656a5
Michel Miladinovic <miladino77@hotmail.com>
2017-06-14 15:00:19 +0200

    Adds CreateContentViewModel

---
+++ MyEpiserverSite/Models/ViewModels/CreateContentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyEpiserverSite.Models.ViewModels
+{
+    public class CreateContentViewModel
+    {
+        public string Text { get; set; }
+    }
+}
