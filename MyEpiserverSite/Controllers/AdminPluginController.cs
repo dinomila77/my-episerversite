4d0a7a7b3e57e59acd7b6b3d48a52ad4276c0c30
Michel Miladinovic <miladino77@hotmail.com>
2017-06-20 16:40:13 +0200

    Creates basic gui plugin

---
+++ MyEpiserverSite/Controllers/AdminPluginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer.PlugIn;
+using MyEpiserverSite.Models.ViewModels;
+
+namespace MyEpiserverSite.Controllers
+{
+    [GuiPlugIn(
+            Area = PlugInArea.AdminMenu,
+            Url = "~/AdminPlugin/Index",
+            DisplayName = "My Plugin"
+            )]
+    public class AdminPluginController : Controller
+    {
+        public ActionResult Index()
+        {
+            var model = new CreateContentViewModel {Text = "This is my plugin"};
+            return View("Index",model);
+        }
+    }
+}
