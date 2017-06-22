8ddc67817e99d93f3ab4cf2d9c25346eb44fa86e
Michel Miladinovic <miladino77@hotmail.com>
2017-06-22 09:06:43 +0200

    Creates page under selected in dropdown,but lists id not page names

--- MyEpiserverSite/Controllers/AdminPluginController.cs
+++ MyEpiserverSite/Controllers/AdminPluginController.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.DataAccess;
 using EPiServer.PlugIn;
+using MyEpiserverSite.Models.Pages;
 using MyEpiserverSite.Models.ViewModels;
 
 namespace MyEpiserverSite.Controllers
@@ -15,10 +19,78 @@ namespace MyEpiserverSite.Controllers
             )]
     public class AdminPluginController : Controller
     {
-        public ActionResult Index()
+        //public ActionResult Index(CreateContentPage currentPage)
+        //{
+        //    var model = PageViewModel.Create(currentPage);
+        //    return View("Index",model);
+        //}
+
+        private readonly IContentLoader _contentLoader;
+        private readonly IContentRepository _contentRepository;
+
+        public AdminPluginController(IContentLoader contentLoader, IContentRepository contentRepository)
         {
-            var model = new CreateContentViewModel {Text = "This is my plugin"};
-            return View("Index",model);
+            _contentLoader = contentLoader;
+            _contentRepository = contentRepository;
         }
+
+        public ActionResult Index(string btnCreate)
+        {
+            //var desc = _contentLoader.GetDescendents(ContentReference.StartPage);
+            //var model = new CreateContentViewModel
+            //{
+            //    Text = "This is my plugin",
+            //    ContentReferences = desc
+            //};
+            //var reference = new ContentReference(49);
+            //var content = _contentLoader.Get<IContent>(new ContentReference(49));
+            var pages = _contentLoader.GetChildren<PageData>(ContentReference.StartPage);
+            var model = new CreateContentViewModel
+            {
+                Description = "This is my plugin",
+                Pages = pages,
+                //ParentId = reference
+            };
+
+            //if (btnCreate != null)
+            //{
+            //    Create(model);
+            //}
+
+            return View("Index", model);
+        }
+
+        [HttpPost]
+        public ActionResult Index(CreateContentViewModel model)
+        {
+            
+
+            if (ModelState.IsValid)
+            {                
+                var parent = model.ParentId;
+                StandardPage standardPage = _contentRepository.GetDefault<StandardPage>(parent);
+
+                standardPage.PageName = "Plugin page";
+                standardPage.Introduction = model.Text;
+                _contentRepository.Save(standardPage, SaveAction.Publish);
+                TempData["success"] = "Page created!";
+            }
+
+            return RedirectToAction("Index");
+            //return View("Index", model);
+        }
+
+        //public void Create(CreateContentViewModel model)
+        //{
+        //    //var parent = model.Pages.Select(c => c.ContentLink);
+
+        //    ContentReference parent = model.ParentId;
+        //    StandardPage standardPage = _contentRepository.GetDefault<StandardPage>(parent);
+
+        //    standardPage.PageName = "Plugin page";
+        //    standardPage.Introduction = model.Text;
+        //    _contentRepository.Save(standardPage, SaveAction.Publish);
+        //    TempData["success"] = "Page created!";
+        //}
     }
 }
--- MyEpiserverSite/Models/ViewModels/CreateContentViewModel.cs
+++ MyEpiserverSite/Models/ViewModels/CreateContentViewModel.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using EPiServer.Core;
+using EPiServer.Web;
 
 namespace MyEpiserverSite.Models.ViewModels
 {
     public class CreateContentViewModel
     {
+        public string Description { get; set; }
+
+        [Required]
         public string Text { get; set; }
+        [Required]
+        public ContentReference ParentId { get; set; }
+
+        public IEnumerable<ContentReference> ContentReferences { get; set; }
+       
+        public IEnumerable<PageData> Pages { get; set; }
+
     }
 }
