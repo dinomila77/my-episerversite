73391ae11dc716901a3a25bb1eae5d09cda5df8d
Michel Miladinovic <miladino77@hotmail.com>
2017-06-22 08:54:09 +0200

    Adds Dependency resolver files from Alloy project

---
+++ MyEpiserverSite/Business/DependencyResolverInitialization.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using EPiServer.ServiceLocation;
+
+namespace MyEpiserverSite.Business
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+    public class DependencyResolverInitialization : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            DependencyResolver.SetResolver(new StructureMapDependencyResolver(context.Container));
+        }
+
+        public void Initialize(InitializationEngine context)
+        {
+            //Add initialization logic, this method is called once after CMS has been initialized
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+
+        public void Preload(string[] parameters)
+        {
+        }
+    }
+}
