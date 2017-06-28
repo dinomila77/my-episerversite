3a51ce05bd05745bd76b8fbe1c845ce88d423c81
Michel Miladinovic <miladino77@hotmail.com>
2017-06-28 14:27:42 +0200

    Appends created page name with numbers if name exists

--- MyEpiserverSite/Controllers/CreateContentPageController.cs
+++ MyEpiserverSite/Controllers/CreateContentPageController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web.Mvc;
 using EPiServer;
 using EPiServer.Core;
@@ -17,20 +18,6 @@ namespace MyEpiserverSite.Controllers
     [TemplateDescriptor(Default = true)]
     public class CreateContentPageController : PageControllerBase<CreateContentPage>
     {
-        //public PageData CurrentPage
-        //{
-        //    get
-        //    {
-        //        var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
-        //        return pageRouteHelper.Page;
-        //    }
-        //}
-        //public void GetCurrent()
-        //{
-        //    var pageRouteHelper = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<EPiServer.Web.Routing.IPageRouteHelper>();
-            
-        //}
-
         public ActionResult Index(CreateContentPage currentPage)
         {
             var model = PageViewModel.Create(currentPage);
@@ -45,19 +32,59 @@ namespace MyEpiserverSite.Controllers
 
         [HttpPost]
         public ActionResult SubmitForm(UserBasicViewModel model, CreateContentPage currentPage)
-        { 
-            if (!ModelState.IsValid) return PartialView("CreateContent",model);
+        {
+            if (!ModelState.IsValid) return PartialView("CreateContent", model);
             //if (currentPage.ParentId == null) return PartialView("CreateContent", model);
             try
             {
+                string pageName = "New page";
                 ContentReference parent = currentPage.ParentId;
-                IContentRepository contentRepository = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<IContentRepository>();
-                StandardPage standardPage = contentRepository.GetDefault<StandardPage>(parent);
+                IContentRepository repository = EPiServer.ServiceLocation.ServiceLocator.Current.GetInstance<IContentRepository>();
+                var children = repository.GetChildren<PageData>(parent).ToList();
 
-                standardPage.PageName = "New page";
-                standardPage.Introduction = $"{model.Email} {model.PersonalNumber}";
+                StandardPage standardPage = repository.GetDefault<StandardPage>(parent);
+                //The code example below shows how to define the user defined property MainBody:
+                standardPage.Property["MainBody"].Value = $"<p>{model.Email}</p> <p>{model.PersonalNumber}</p>";
 
-                contentRepository.Save(standardPage,SaveAction.Publish);
+                //The following case achieves the same with a strongly typed model:
+                //standardPage.MainBody = new XhtmlString($"Strong typed <p>{model.Email}</p> <p>{model.PersonalNumber}</p>");
+
+                char[] delimiterChars = { '(', ')' };
+                if (children.Any() && children.Exists(p => p.Name == pageName))
+                {
+                    var s = pageName.Split(delimiterChars);
+
+                    //var nameList = children.FindAll(p=> p.Name.Split('(').ToString() == s[0]);
+                    //var tempList = children.SelectMany(p => p.Name.Split(delimiterChars,StringSplitOptions.RemoveEmptyEntries));
+                    var tempList = new List<PageData>();
+                    foreach (var pageData in children)
+                    {
+                        var split = pageData.Name.Split(delimiterChars);
+                        var name = split[0];
+                        if (name == pageName)
+                        {
+                            tempList.Add(pageData);
+                        }
+                    }
+
+                    if (!HasInteger(tempList))
+                    {
+                        standardPage.Name = pageName += "(2)";
+                        repository.Save(standardPage, SaveAction.Publish);
+                    }
+                    else
+                    {
+                        standardPage.Name = GetInteger(tempList);
+                        repository.Save(standardPage, SaveAction.Publish);
+                    }
+                }
+                else
+                {
+                    standardPage.Name = pageName;
+                    repository.Save(standardPage, SaveAction.Publish);
+                }
+
+
                 ModelState.Clear();
                 TempData["success"] = true;
                 return PartialView("CreateContent");
@@ -67,6 +94,62 @@ namespace MyEpiserverSite.Controllers
                 ModelState.AddModelError("error", "Something went wrong... " + ex.Message);
                 return PartialView("CreateContent");
             }
+        }
+
+        private static bool HasInteger(List<PageData> childPages)
+        {
+            foreach (var childPage in childPages)
+            {
+                var charArray = childPage.Name.ToCharArray();
+                foreach (var c in charArray)
+                {
+                    if (char.IsDigit(c))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static bool HasInteger(string page)
+        {
+            var charArray = page.ToCharArray();
+            foreach (var c in charArray)
+            {
+                if (char.IsDigit(c))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private static string GetInteger(List<PageData> childPages)
+        {
+            var myNrs = new List<string>();
+            var myList = new List<string>();
+            char[] delimiterChars = { '(', ')' };
+            foreach (var childPage in childPages)
+            {
+                if (HasInteger(childPage.Name))
+                    myList.Add(childPage.Name);
+            }
+
+            foreach (var s in myList)
+            {
+
+                var nr = s.Split(delimiterChars);
+                myNrs.Add(nr[1]);
+
+            }
+
+            var max = myNrs.OrderByDescending(v => int.Parse(v.Substring(0))).First();
+            var max2 = myNrs.Select(v => int.Parse(v.Substring(0))).Max();
+            var strArray = myList.Select(s => s.Split(delimiterChars)).FirstOrDefault();
+            string result = strArray[0] += $"({max2 + 1})";
+
+            return result;
         }
     }
 }
